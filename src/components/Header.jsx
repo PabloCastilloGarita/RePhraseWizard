fba12c6018a5214e26dc9f048f1b0f3ab1ae7073
Pablo Castillo <pcastillogarita@gmail.com>
2023-06-21 13:33:19 -0600

    feat: :sparkles: add nav and headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import { logo } from '../assets'
 
 const Header = () => {
-  return (
-    <header>
-      <nav>
-        <img src={logo} alt="logo" />
-        <button>Github</button>
-      </nav>
-    </header>
-  )
+	return (
+		<header className='w-full flex justify-center items-center flex-col'>
+			<nav className='flex justify-between items-center w-full'>
+				<img src={logo} alt="logo" className='h-40 w-52 ml-8 object-contain' />
+				<button className='github_btn mr-8'>
+					Github
+				</button>
+			</nav>
+			<h1 className='head_text'>Rephrase text with <br /> AI Intelligence</h1>
+			<h2 className='desc'>
+				You no longer need to spend valuable time and energy grappling with complex sentence structures or struggling to find alternative ways to express your ideas. Our advanced algorithm and intuitive interface empower you to seamlessly rephrase any piece of text
+			</h2>
+		</header>
+	)
 }
 
 export default Header
