f18c711914f256c9583b5b43b3c852c83a4964b2
Pablo Castillo <pcastillogarita@gmail.com>
2023-06-21 12:11:51 -0600

    feat: :sparkles: add Header & Hero components
    setting up the components folder by adding Header and Hero files, that are going to be my only files from now

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="" alt="logo" />
+        <button>Github</button>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
