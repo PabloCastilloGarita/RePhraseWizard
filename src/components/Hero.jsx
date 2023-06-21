ce48470b7b53649605bf957cfa8ee265e9b7ade4
Pablo Castillo <pcastillogarita@gmail.com>
2023-06-21 14:30:28 -0600

    feat: :sparkles: add Hero layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='mt-10'>
+      <ul className='flex justify-center items-center my-3'>
+        <li className='mr-3'><button className='language_btn'>English</button></li>
+        <li><button className='language_btn'>Spanish</button></li>
+      </ul>
+
+      <div className='mx-36 grid grid-cols-2'>
+
+        {/* Input Text */}
+        <div className='bg-gray-200 rounded-t-lg mr-3'>
+          <form action="">
+            <textarea
+              cols="20" rows="10"
+              className='text_area'
+              required
+              placeholder='Enter your text and let the magic begin!'
+              maxLength={250}
+            />
+            <button type='submit' className='github_btn'>Wizardize</button>
+          </form>
+        </div>
+
+        {/* Result */}
+        <div className='bg-gray-200 rounded-t-lg h-full'>
+          <p>
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
