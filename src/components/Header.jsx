652c282a932510aea868566518a6aed9a280205b
Pablo Castillo <pcastillogarita@gmail.com>
2023-06-21 12:53:47 -0600

    chore: :bento: add logo.svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { logo } from '../assets'
 
 const Header = () => {
   return (
     <header>
       <nav>
-        <img src="" alt="logo" />
+        <img src={logo} alt="logo" />
         <button>Github</button>
       </nav>
     </header>
